6e7b2333031da6fac5d0b7bbe1f2667115cd5e44
Cheng Zhao <zcbenz@gmail.com>
2017-04-05 18:02:06 +0900

    Fix more API changes due to name changes

--- atom/browser/native_window_mac.mm
+++ atom/browser/native_window_mac.mm
@@ -1418,7 +1418,7 @@ void NativeWindowMac::RefreshTouchBarItem(const std::string& item_id) {
 }
 
 void NativeWindowMac::OnInputEvent(const blink::WebInputEvent& event) {
-  switch (event.type) {
+  switch (event.type()) {
     case blink::WebInputEvent::GestureScrollBegin:
     case blink::WebInputEvent::GestureScrollUpdate:
     case blink::WebInputEvent::GestureScrollEnd:
